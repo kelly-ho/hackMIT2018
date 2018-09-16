71322ca6f472740d0e9bb8d28433605b8598fc72
kelly-ho <43300542+kelly-ho@users.noreply.github.com>
2018-09-16 00:45:03 -0400

    Update image.py

--- image.py
+++ image.py
@@ -1,11 +1,9 @@
-import cv2, os, math, numpy, scipy.stats
+import cv2, os, math, scipy.stats
 
-def getImageAbsorbence(imgName):
+def getImageAbsorbence(imgName, i0):
     img = cv2.imread("Pictures/"+imgName)
     height = img.shape[0]
     width = img.shape[1]
-
-    print(height, width)
 
     imgSeg = img[height//2-int(0.05*height):height//2+int(0.05*height), width//2-int(0.05*width):width//2+int(0.05*width)]
     cv2.imwrite("croppedImage.jpg", imgSeg)
@@ -13,30 +11,53 @@ def getImageAbsorbence(imgName):
     #order is blue, green, red
     average_color = [imgSeg[:, :, i].mean() for i in range(img.shape[-1])]
 
-    print(average_color)
-
     finalColorRead = max(average_color)
     color = average_color.index(finalColorRead)
     gbr = {0:"blue", 1:"green", 2:"red"}
     print("Avg", gbr[color],"color:", finalColorRead)
 
-    i0 = 255
     i = finalColorRead
     absorbence = math.log10(i/i0)
     return absorbence
 
+def getI0(imgName):
+    img = cv2.imread("Pictures/"+imgName)
+    height = img.shape[0]
+    width = img.shape[1]
+
+    imgSeg = img[height//2-int(0.05*height):height//2+int(0.05*height), width//2-int(0.05*width):width//2+int(0.05*width)]
+    cv2.imwrite("croppedImage.jpg", imgSeg)
+
+    average_color = [imgSeg[:, :, i].mean() for i in range(img.shape[-1])]
+
+    finalColorRead = max(average_color)
+    return (finalColorRead)
+
 imgNames = os.listdir('Pictures/.')
 print(imgNames)
-calibration = {}
+
+for i,a in enumerate(imgNames):
+    print ('%d - %s' % (i, a))
+
+waterIndex = int(input("Water image: "))
+waterFile = imgNames[waterIndex]
+imgNames.remove(imgNames[waterIndex])
+print(imgNames)
+
+absorbs = []
+concens = []
+
+i0 = getI0(waterFile)
 
 for img in range(0, len(imgNames)):
-    conc = input("Concentration of sample "+imgNames[img] + ": ")
-    absorb = getImageAbsorbence(imgNames[img])
-    calibration[absorb] = conc
+    concen = float(input("Concentration of sample "+imgNames[img] + ": "))
+    concens.append(concen)
+    absorbs.append(getImageAbsorbence(imgNames[img], i0))
 
-print(calibration)
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(absorbs, concens)
+print("slope:", slope)
+print("intercept", intercept)
 
-x = numpy.array(calibration.keys())
-y = numpy.array(calibration.values())
-
-slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x,y)
+unknown = float(input("Unknown sample's absorbance: "))
+unknownConc = (unknown - intercept) / slope
+print(unknownConc)
